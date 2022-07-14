37545feeae61a8399f18cc08e5ca3cc1bdb7a918
arafat-alim <arafat97alim@gmail.com>
2022-07-14 21:43:48 +0530

    end game part 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { nanoid } from "nanoid";
 import React from "react";
+import Confetti from "react-confetti";
 import Die from "./components/Die";
 
 function App() {
@@ -64,7 +65,7 @@ function App() {
   }
   return (
     <main>
-      {/* {tenzies && } */}
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
@@ -72,7 +73,7 @@ function App() {
       </p>
       <div className="dice-container">{diceElement}</div>
       <button onClick={rollDice} className="roll-dice">
-        {tenzies ? "Reset" : "Roll"}
+        {tenzies ? "New Game" : "Roll"}
       </button>
     </main>
   );
