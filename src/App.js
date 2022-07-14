4e3e797ab19c2e68f5d858ad6b31361e27800460
arafat-alim <arafat97alim@gmail.com>
2022-07-14 18:59:26 +0530

    add roll button and changing the dice input randomly.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import React from "react";
 import Die from "./components/Die";
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+  //function to create random 10 digit number in an array
+  function allNewDice() {
+    const newArr = [];
+    for (let i = 0; i < 10; i++) {
+      newArr.push(Math.ceil(Math.random() * 6));
+    }
+    return newArr;
+  }
+  const diceElement = dice.map((die) => {
+    return <Die value={die} />;
+  });
+
+  function onRoll() {
+    setDice(allNewDice);
+  }
   return (
     <main>
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        {diceElement}
+        <button onClick={onRoll}>Roll</button>
       </div>
     </main>
   );
